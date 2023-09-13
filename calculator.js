7976943dc7152105df39f37a8a4ab9f95c817305
ummenusra0 <ummaynusra@gmail.com>
2023-09-13 11:30:09 +0600

    added new commits

--- calculator.js
+++ calculator.js
@@ -3,24 +3,30 @@ document.getElementById('calculator-field').addEventListener('click', function()
     const incomeField = document.getElementById('income-field')
     const newIncomeAmountString = incomeField.value;
     const newIncomeAmount = parseFloat(newIncomeAmountString)
-    console.log(newIncomeAmount);
 
 
     const expensesField = document.getElementById('expenses-field')
     const newExpensesAmountString = expensesField.value;
     const newExpensesAmount = parseFloat(newExpensesAmountString)
-    console.log(newExpensesAmount);
 
     
     const rentField = document.getElementById('rent-field')
     const newRentAmountString = rentField.value;
     const newRentAmount = parseFloat(newRentAmountString)
-    console.log(newRentAmount);
 
 
     const clotheField = document.getElementById('clothes-filed')
     const newClotheAmountString = clotheField.value;
     const newClotheAmount = parseFloat(newClotheAmountString)
-    console.log(newClotheAmount);
+
+
+    const expensesTotalAmount = newExpensesAmount + newRentAmount + newClotheAmount;
+
+
+    document.getElementById('total-expenses').innerText = expensesTotalAmount;
+
+    const totalBalanceAmount = newIncomeAmount - expensesTotalAmount;
+    document.getElementById('balance-calculation').innerText = totalBalanceAmount;
+
 
 })
